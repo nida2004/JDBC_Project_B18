ccdae6770f202723f14c8dce1b8dde45910a25a0
nida2004 <nyilmaz2025@gmail.com>
2020-10-13 22:51:20 +0200

    commit nida

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,11 @@
+package add_file_here;
+
+public class EU3_Practice {
+    public static void main(String[] args) {
+        System.out.println( "EU3 openning pullrequest");
+        //commit your change
+        //commit your change
+        //push with nice commit masseage
+    }
+
+}
--- src/test/java/add_file_here/SampleFile.java
+++ src/test/java/add_file_here/SampleFile.java
@@ -5,6 +5,8 @@ public class SampleFile {
     public static void main(String[] args) {
 
         System.out.println("Awesome stuff here");
+        System.out.println("Hello new");
+        System.out.println("hello new");
 
     }
 
